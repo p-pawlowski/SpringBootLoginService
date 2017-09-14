4b6d72bcc59227c0ba2100ea127b065c3c0d3506
p-pawlowski <piotr.m.pawlowski@gmail.com>
2017-09-14 20:10:40 +0200

    ModelAndView -> Model

--- src/main/java/com/example/demo/controller/DefaultController.java
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -1,16 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
 	
 	@RequestMapping(value="/access-denied")
-	public ModelAndView accessDenied(){
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("access-denied");
-		return modelAndView;
+	public String accessDenied(Model model){
+		return "access-denied";
 	}
 }
